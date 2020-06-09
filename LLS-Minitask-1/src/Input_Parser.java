f76337b2add243afc080657b5ff024eb2012adbe
Pallavi G R <pallavi.gr3@gmail.com>
2020-06-09 18:10:07 +0200

    input to array- to read

--- LLS-Minitask-1/src/Input_Parser.java
+++ LLS-Minitask-1/src/Input_Parser.java
@@ -4,10 +4,6 @@ import java.util.*;
 
 public class Input_Parser {
 
-	public Input_Parser() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args)throws IOException {
 		/*try  
 		{  
@@ -25,25 +21,45 @@ public class Input_Parser {
 		{  
 		e.printStackTrace();  
 		}  */
+		String str_data = null;
+		String file_name = "C:\\Users\\Pallavi GR\\git\\LLS-MiniTask-1\\LLS-Minitask-1\\data\\aiger-set\\ascii\\aig_0_min.aag";
 		
-		FileReader fr = new FileReader("C:\\Users\\Pallavi GR\\git\\LLS-MiniTask-1\\LLS-Minitask-1\\data\\aiger-set\\ascii\\aig_0_min.aag");
-		BufferedReader br = new BufferedReader(fr);
-		//int numberOfLines = 3;
-		String aLine;
-		int numberoflines = 0;
-		
-		while((aLine = br.readLine())  != null) {
-			numberoflines++;
+			
+			StringBuilder sb = new StringBuilder();
+		    String strLine = "";
+		    List<String> list = new ArrayList<String>();
+	  
+			FileReader fr = new FileReader(file_name);
+			BufferedReader br = new BufferedReader(fr);
+			try {  		
+	
+			// converts the input to the contents of the array
+			while (strLine != null )
+	        {
+				strLine = br.readLine();
+                sb.append(strLine);
+                sb.append(System.lineSeparator());
+                strLine = br.readLine();
+                if (strLine==null)
+                   break;
+                list.add(strLine);
+            }
+         System.out.println(Arrays.toString(list.toArray()));
+	   } catch (FileNotFoundException e) {
+	       System.err.println("File not found");
+	   } catch (IOException e) {
+	       System.err.println("Unable to read the file.");
+	   }
+			
+			
+		ReadFile file = new ReadFile(file_name);   //object of ReadFile
+			String[] aryLines = file.OpenFile();
+			int i;
+			for(i=0;i<aryLines.length;i++) {		
+				System.out.println(aryLines[i]);	
+				}			
 		}
-		String[ ] textData = new String[numberOfLines];
-		int i;
-
-		for (i=0; i < numberOfLines; i++) {
-		textData[ i ] = br.readLine();
-
-		}
-		br.close( );
-
-	}
-
 }
+
+
+
---
+++ LLS-Minitask-1/src/ReadFile.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReadFile {
+
+	private String Path;
+	
+	public ReadFile(String file_path) {
+		Path = file_path;
+	}
+	
+	public String[] OpenFile() throws IOException{
+		FileReader fr = new FileReader(Path);
+		BufferedReader br = new BufferedReader(fr);
+		
+		//Gives the number of Lines.
+		int numberOfLines = readLines();
+		String[ ] textData = new String[numberOfLines];
+		String [] arr = new String[numberOfLines];
+		int i;
+
+		for (i=0; i < numberOfLines; i++) {
+		arr[ i ] = br.readLine();
+		//textData[i] = textData[i] + arr[i];
+		//System.out.println(textData[i]);
+		}
+		br.close( );
+		//return textData;
+		return arr;
+
+	}
+//NUMBER OF LINES
+	int readLines() throws IOException{
+		FileReader ftr = new FileReader(Path);
+		BufferedReader bf = new BufferedReader(ftr);
+		
+		String aLine;
+		int numberofLines = 0;
+		
+		while (( aLine = bf.readLine()) != null) {
+			numberofLines++;
+		}
+		bf.close();
+		return numberofLines;  // returns number of lines
+	}
+}
+	
