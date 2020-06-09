819265017aab64ada46d7eefadb36b1a640e87d7
Lukas Rothenberger <lukas_rothenberger@hotmail.de>
2020-06-09 14:37:52 +0200

    minor change (performing equivalence checks in main)

--- LLS-Minitask-1/src/main/Main.java
+++ LLS-Minitask-1/src/main/Main.java
@@ -45,6 +45,10 @@ public class Main {
 		graph.exportToDOTandPNG("majGraph");
 		graph.exportToBLIF("majGraph");
 
+		//#### Perform Equivalence checks:
+		//input file <-> created unmodified Graph
+		ABC.EquivalenceCheck.performEquivalenceCheck(new File("data/aiger-set/aig_0_min.blif"), new File("output/unmodifiedGraph.blif"));
+		//created unmodified Graph <-> MAJ Graph
 		ABC.EquivalenceCheck.performEquivalenceCheck(new File("output/majGraph.blif"), new File("output/unmodifiedGraph.blif"));
 	}
 	
